89a6b621f5c2f022e0edaae18d1af0fd6b20a86a
Fredric Gluck <fbgluck@gmail.com>
2023-05-19 09:36:02 -0400

    Various Changes and updates

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,23 +1,4 @@
-# Age is the age of the customer
-age = 23
-# Check to see if the customer can be served
-if age < 21:
-    print("Underage")  # Underaged people can not be served
-else:
-    print("OK to serve")
-# Test to see if a number is even or odd
-test = int(input("What is the number you want to test?"))
-if test % 2 == 0:
-    print("the number", test, "is even")
-else:
-    print("the number", test, "is odd")
 
-
-# Order of Operations
-a = 5
-b = 6
-c = 7
-# Why do I get different results
-print("Without ():", a*b+c)
-print("With ():", (a*b)+c)
-print("And this:", a*(b+c))
+from datetime import date
+d = (date(2022, 12, 25))
+print(d)
--- working_with_functions.py
+++ working_with_functions.py
@@ -1,27 +1,72 @@
-#functions 
+# functions
 # ref: https://www.programiz.com/python-programming/function
 # define a function
+# The program consists of calling each function
+# Author: Fredric Gluck
+# ------------------------------
+# Variables
+
+
+# Functions
+def mr_glucks_function(x):
+    result = ((2*x)-8)**(2/3)
+    return result
+# End of Function
+
+# Define a  basic function to calculate the area of a rectangle
+# Arguments: length and width of the field
+
+
+def fn_field_area(area_length, area_width):
+    return (area_length * area_width)
+
+
 def absolute_value(num):
     # The following is a doc string. It is used to
     # document the purpose of a function.
     # from the Python interpreter, type -- print (absolute_value.__doc__)
     """This function returns the absolute
     value of the entered number"""
-    
+
     print("\nAbsolute Value Function")
     if num >= 0:
         return num
     else:
         return -num
-#End of the function because the next line is not indented
+# End of the function because the next line is not indented
 
-def fancy_av (prompt, num):
-    print ("\nFancy Absolute Value Function")
+# A fancier version of the absolute value function
+
+
+def fancy_av(prompt, num):
+    print("\nFancy Absolute Value Function")
     if num >= 0:
         x = prompt + " " + str(num)
         return x
     else:
         return -num
 
-print(fancy_av("\nThe absolute value is: ",2))
+
+# ----------End of Function Defs ----------------------------
+#  Main Program Starts Here
+print("Mr. Gluck's Function")
+for i in range(0, 11):
+    print(f"The input is: {i}....The result is {mr_glucks_function(i)}")
+input("Enter to continue ...")
+print(30*'-')
+
+
+# Ask the user for the length and width
+# Call the function and return the value
+user_length = int(input("What is the length of the field? >> "))
+user_width = int(input("What is the width of the field? >> "))
+print(f'Calling function from main program')
+# We call the function here
+total_area = fn_field_area(user_length, user_width)
+print(f'We are back in the main program')
+print(f'The area of the field is: {total_area}')
+print(30*'-')
+input('Enter to continue....')
+# Now we will call the other functions
+print(fancy_av("\nThe absolute value is: ", 2))
 print(absolute_value(-4))
